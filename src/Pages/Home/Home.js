de44ad8f9912900a74f7e295f94d9db8ba2ba9ab
gabriel moslaves <gabrielmoslaves@gmail.com>
2023-06-28 19:43:25 -0300

    init

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../../Sections/About/About";
+import Products from "../../Sections/Products/Products";
+import About from "../../Sections/About/About";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Products />
+      <About />
+    </div>
+  );
+};
+
+export default Home;
