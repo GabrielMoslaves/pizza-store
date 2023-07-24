c087f349887aee20fd07dbe59f2aa94c2c8eecd2
gabriel moslaves <gabrielmoslaves@gmail.com>
2023-07-24 00:30:23 -0300

    increase features in modal

--- src/Sections/CartModal/index.js
+++ src/Sections/CartModal/index.js
@@ -2,10 +2,24 @@ import styles from "./styles.module.scss";
 import { useOpener } from "../../hooks/useOpener";
 import { useSelector } from "../../hooks/useSelector";
 import Button from "../../components/Button/Button";
+import Swal from "sweetalert2";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import {
+  Box,
+  FormControl,
+  Input,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+} from "@mui/material";
+import { useState } from "react";
 
 const CartModal = () => {
   const { setOpenModal } = useOpener();
-  const { selectedProducts } = useSelector();
+  const { selectedProducts, setSelectedProducts } = useSelector();
+  const [paymentForm, setPaymentForm] = useState("");
 
   const handleOutsideClick = (event) => {
     if (event.target === event.currentTarget) {
@@ -13,39 +27,217 @@ const CartModal = () => {
     }
   };
 
-  const totalPrice = selectedProducts.reduce(
-    (accumulator, item) => accumulator + item.price,
+  const parsedSelectedProducts = selectedProducts.map((item) => {
+    return { ...item, totalItemPrice: item.qtd * item.price };
+  });
+
+  const totalPrice = parsedSelectedProducts.reduce(
+    (accumulator, item) => accumulator + item.totalItemPrice,
     0
-  );    
+  );
 
+  const decreaseItem = (item) => {
+    if (item.qtd > 1) {
+      const count = (item.qtd = item.qtd - 1);
+      const newArray = selectedProducts.map((i) => {
+        if (i.id === item.id) {
+          return { ...i, qtd: count };
+        } else {
+          return i;
+        }
+      });
+      setSelectedProducts(newArray);
+    } else {
+      Swal.fire({
+        title: "Deseja remover este item?",
+        text: "",
+        icon: "question",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonText: "Cancelar",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Sim, remova!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          setSelectedProducts((prevState) =>
+            prevState.filter((i) => i.id !== item.id)
+          );
+          Swal.fire(
+            "Removido!",
+            "O item foi removido do seu carrinho.",
+            "success"
+          );
+        }
+      });
+    }
+  };
+
+  const addItem = (item) => {
+    const count = (item.qtd = item.qtd + 1);
+    const newArray = selectedProducts.map((i) => {
+      if (i.id === item.id) {
+        return { ...i, qtd: count };
+      } else {
+        return i;
+      }
+    });
+
+    setSelectedProducts(newArray);
+  };
+
+  const handleSubmit = () => {
+    if (needChange && change < totalPrice) {
+      Swal.fire({
+        icon: "error",
+        title: "Ops...",
+        text: "Valor do troco inferior ao total do pedido!",
+      });
+    } else {
+      Swal.fire({
+        title: "Deseja finalizar o pedido?",
+        text: "",
+        icon: "question",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonText: "Revisar pedido",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Sim!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            "Pedido enviado!",
+            "Dentro de 50 minutos o pedido será entregue.",
+            "success"
+          );
+          setOpenModal(false);
+          setSelectedProducts([]);
+          console.log(selectedProducts);
+        }
+      });
+    }
+  };
+
+  const handleChange = (event) => {
+    setPaymentForm(event.target.value);
+  };
+  const [change, setChange] = useState(0);
+  const [needChange, setNeedChange] = useState(false);
   return (
-    <div className={styles.container} onClick={handleOutsideClick}>
-      <div className={styles.content}>
+    <div className={styles.background} onClick={handleOutsideClick}>
+      <div className={styles.carrinhoContainer}>
         <h1>Produtos Selecionados</h1>
-
-        <table>
-          <tr>
-            <td>Produto</td>
-            <td>Quantidade</td>
-            <td>Valor</td>
-            <td>Observações</td>
-          </tr>
-          {selectedProducts.map((item, index) => {
-            return (
-              <tr key={index}>
-                <td>
-                  <span>{item.name}</span>
-                  <img alt="product" src={item.image} />
-                </td>
-                <td>{item.qtd}</td>
-                <td>{item.price}</td>
-                <input type="text" />
-              </tr>
-            );
-          })}
+        <table className={styles.carrinhoTable}>
+          <thead>
+            <tr>
+              <th>Produto</th>
+              <th>Quantidade</th>
+              <th>Valor</th>
+              <th>Observações</th>
+            </tr>
+          </thead>
+          <tbody>
+            {selectedProducts?.map((item) => {
+              return (
+                <tr>
+                  <td>
+                    <div className={styles.nomeProduto}>{item.name}</div>
+                    <img
+                      alt="Pizza"
+                      className={styles.imagemProduto}
+                      src={item.image}
+                    />
+                  </td>
+                  <td>
+                    <div className={styles.quantidade}>
+                      <button
+                        onClick={() => decreaseItem(item)}
+                        className={styles.botaoQuantidade}
+                      >
+                        -
+                      </button>
+                      <span>{item.qtd}</span>
+                      <button
+                        onClick={() => addItem(item)}
+                        className={styles.botaoQuantidade}
+                      >
+                        +
+                      </button>
+                    </div>
+                  </td>
+                  <td>
+                    <span className={styles.valor}>
+                      R$ {(item.qtd * item.price).toFixed(2)}
+                    </span>
+                  </td>
+                  <td>
+                    <TextField
+                      id="standard-multiline-flexible"
+                      multiline
+                      maxRows={4}
+                      variant="standard"
+                      placeholder="Remover cebola, trocar molho, etc..."
+                      onChange={(e) => console.log(e.target.value)}
+                    />
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
         </table>
-        <p>total: {totalPrice}</p>
-        <Button text="Finalizar" />
+        <div className={styles.footer}>
+          <div className={styles.valor}>TOTAL: R$ {totalPrice.toFixed(2)}</div>
+          <div style={{ minWidth: "200px" }}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">
+                Forma de pagamento
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                label="Forma de pagamento"
+                value={paymentForm}
+                onChange={handleChange}
+              >
+                <MenuItem value="Cartão de crédito">Cartão de crédito</MenuItem>
+                <MenuItem value="Dinheiro">Dinheiro</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+          {console.log(needChange)}
+          {paymentForm === "Dinheiro" && (
+            <div style={{ display: "flex", gap: "20px" }}>
+              <p>Precisa de troco?</p>
+              <div style={{ display: "flex", gap: "5px" }}>
+                <input
+                  checked={needChange}
+                  type="radio"
+                  onChange={(e) => setNeedChange(e.target.checked)}
+                />
+                <span>Sim</span>
+              </div>
+              <div style={{ display: "flex", gap: "5px" }}>
+                <input
+                  checked={!needChange}
+                  type="radio"
+                  onChange={(e) => setNeedChange(!e.target.checked)}
+                />
+                <span>Não</span>
+              </div>
+            </div>
+          )}
+          {needChange && paymentForm === "Dinheiro" && (
+            <TextField
+              onChange={(e) => setChange(e.target.value)}
+              placeholder="Troco para quanto?"
+              id="standard-size-small"
+              size="small"
+              variant="standard"
+            />
+          )}
+        </div>
+        <div style={{ paddingTop: "10px" }}>
+          <Button onClick={() => handleSubmit()} text="Finalizar" />
+        </div>
       </div>
     </div>
   );
